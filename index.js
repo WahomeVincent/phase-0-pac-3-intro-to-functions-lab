a3532cc98f67e11311c21787ec9c14e5bc307346
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-12 19:03:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+
+function logShout(string) {
+    console.log("HELLO")
+}
+
+function logWhisper(string) {
+    console.log("hello")
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    return "i can't hear you!"
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    return "YES INDEED!"
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    return "I would love to!"
+}
